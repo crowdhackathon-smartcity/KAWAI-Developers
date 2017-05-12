b17d9a508b5545e2a5bb6cbe9353971127298915
Dimitrios Desyllas <ddesillas@freemail.gr>
2017-05-12 17:25:27 +0300

    Add Doctrine Orm Entitied for Basic Application Components

--- webInterface/src/AppBundle/Entity/PersonInNeed.php
+++ webInterface/src/AppBundle/Entity/PersonInNeed.php
@@ -1,7 +1,61 @@
 <?php
+
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
-class PersonInNeed{
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="person_in_need")
+ */
+class PersonInNeed 
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+	
+	/**
+	 * The reason why person is in need
+	 * @ORM\Column(type="integer",name="reason")
+	 */
+	private $reason;
+	
+	
 
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set reason
+     *
+     * @param integer $reason
+     *
+     * @return PersonInNeed
+     */
+    public function setReason($reason)
+    {
+        $this->reason = $reason;
+
+        return $this;
+    }
+
+    /**
+     * Get reason
+     *
+     * @return integer
+     */
+    public function getReason()
+    {
+        return $this->reason;
+    }
 }
---
+++ webInterface/src/AppBundle/Entity/VendingMachine.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="vending_machine")
+ */
+class VendingMachine 
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+	
+	/**
+	 * @ORM\Column(type="string",name="machine_name",length=140)
+	 */
+	private $machine_name;
+	
+	/**
+	 * @ORM\Column(type="string",name="key",length=140)
+	 */
+	private $key;
+	
+	/**
+	 * @ORM\Column(type="string",name="secret",length=255)
+	 */
+	private $secret;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set machineName
+     *
+     * @param string $machineName
+     *
+     * @return VendingMachine
+     */
+    public function setMachineName($machineName)
+    {
+        $this->machine_name = $machineName;
+
+        return $this;
+    }
+
+    /**
+     * Get machineName
+     *
+     * @return string
+     */
+    public function getMachineName()
+    {
+        return $this->machine_name;
+    }
+
+    /**
+     * Set key
+     *
+     * @param string $key
+     *
+     * @return VendingMachine
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    /**
+     * Get key
+     *
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * Set secret
+     *
+     * @param string $secret
+     *
+     * @return VendingMachine
+     */
+    public function setSecret($secret)
+    {
+        $this->secret = $secret;
+
+        return $this;
+    }
+
+    /**
+     * Get secret
+     *
+     * @return string
+     */
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+}
