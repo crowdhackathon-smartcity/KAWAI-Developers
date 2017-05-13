c6627f8561bc52fd51d7aba4d8dc3a189a21815d
Dimitrios Desyllas <ddesillas@freemail.gr>
2017-05-13 13:37:39 +0300

    Add a new person In Need

---
+++ webInterface/src/AppBundle/Constants/PersonInNeedConstats.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppBundle\Constants;
+
+class PersonInNeedConstats 
+{
+	const PERSON_IN_NEED_NAME='name';
+	const PERSON_IN_NEED_SURNAME='surname';
+	const PERSON_IN_NEED_REASON='reason';
+}
--- webInterface/src/AppBundle/Forms/PersonInNeedType.php
+++ webInterface/src/AppBundle/Forms/PersonInNeedType.php
@@ -6,13 +6,31 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use AppBundle\Entity\PersonInNeed;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Constants\PersonInNeedConstats;
 
 class PersonInNeedType extends AbstractType 
 {
+	/**
+	 * {@inheritDoc}
+	 * @see \Symfony\Component\Form\AbstractType::buildForm()
+	 */
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
-		$builder->add('name',TextType::class,array('label'=>'social.person_in_need.name','placefolder'=>'social.person_in_need.name'))
-		->add('surname',TextType::class,array('label'=>'social.person_in_need.name','placefolder'=>'social.person_in_need.name'))
-		->add('reason',TextareaType::class,array('label'=>'social.person_in_need.reason','placefolder'=>'social.person_in_need.reason'));
+		$builder->add(PersonInNeedConstats::PERSON_IN_NEED_NAME,TextType::class,array('label'=>'social.person_in_need.name'))
+		->add(PersonInNeedConstats::PERSON_IN_NEED_SURNAME,TextType::class,array('label'=>'social.person_in_need.name'))
+		->add(PersonInNeedConstats::PERSON_IN_NEED_REASON,TextareaType::class,array('label'=>'social.person_in_need.reason'));
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \Symfony\Component\Form\AbstractType::configureOptions()
+	 */
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(array(
+				'data_class' => PersonInNeed::class,
+		));
 	}
 }
